078609df4742972edd0a2d730a9ac911b236a55e
Zac Zhibang Jiang <zhibang.jiang@gmail.com>
2019-10-30 17:58:09 -0400

    Update sketch.js

--- 2.mapping-quantities/examples/2.table-from-csv/sketch.js
+++ 2.mapping-quantities/examples/2.table-from-csv/sketch.js
@@ -29,36 +29,6 @@ function setup(){
   var rowHeight = 60
   var colWidth = 40
 
-  // draw country name labels on the left edge of the table
-  textStyle(BOLD)
-  textAlign(RIGHT)
-  for (var c=1; c<table.getColumnCount(); c++){
-    text(table.columns[c], x-colWidth, y)
-    y += rowHeight
-  }
 
-
-  // draw year labels in the header row
-  x = 200
-  y = 100
-  textStyle(NORMAL)
-  textAlign(BOLD)
-  for (var r=0; r<table.getRowCount(); r++){
-    var year = table.getString(r, 0)
-    text(year, x, y-rowHeight)
-    x += colWidth
-  }
-
-  // print out the total for each country, one column at a time
-  x = 200
-  for (var r=0; r<table.getRowCount(); r++){
-    y = 100
-    for (var c=1; c<table.getColumnCount(); c++){
-      var value = table.getNum(r, c)
-      text(value, x, y)
-      y += rowHeight
-    }
-    x += colWidth
-  }
 
 }
