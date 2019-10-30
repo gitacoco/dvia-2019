b2354e068725fb7ffd0456ccae0359bbc538df12
Zac Zhibang Jiang <zhibang.jiang@gmail.com>
2019-10-30 17:52:05 -0400

    Update concept-1.js

--- 2.mapping-quantities/project/concept-1.js
+++ 2.mapping-quantities/project/concept-1.js
@@ -22,6 +22,26 @@ function setup(){
   var rowHeight = 60
   var colWidth = 40
 
+  // draw country name labels on the left edge of the table
+  textStyle(BOLD)
+  textAlign(RIGHT)
+  for (var c=1; c<table.getColumnCount(); c++){
+    text(table.columns[c], x-colWidth, y)
+    y += rowHeight
+  }
+
+
+  // draw year labels in the header row
+  x = 200
+  y = 100
+  textStyle(NORMAL)
+  textAlign(BOLD)
+  for (var r=0; r<table.getRowCount(); r++){
+    var year = table.getString(r, 0)
+    text(year, x, y-rowHeight)
+    x += colWidth
+  }
+
   // print out the total for each country, one column at a time
   x = 200
   for (var r=0; r<table.getRowCount(); r++){
