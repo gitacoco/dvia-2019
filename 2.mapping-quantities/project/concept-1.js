4c74502a865cbafe5ac883bc9f3f310801e22234
Zac Zhibang Jiang <zhibang.jiang@gmail.com>
2019-10-30 17:50:52 -0400

    Update concept-1.js

--- 2.mapping-quantities/project/concept-1.js
+++ 2.mapping-quantities/project/concept-1.js
@@ -1,11 +1,39 @@
-var data
+var totals
 
 function preload(){
-  data = loadJSON('data/totals.json')
-}
-
+  totals = loadTable('data/all-frequency.csv', 'csv', 'header')
 function setup(){
   createCanvas(800, 600)
 
-  print(data)
+  // pick the data file to work with and call it "table"
+  var table = totals
+
+  // log the whole dataset to the console so we can poke around in it
+  print(table)
+
+  // set up typography
+  textFont("Rokkitt")
+  textSize(16)
+  fill(30)
+  noStroke()
+
+  var x = 200
+  var y = 100
+  var rowHeight = 60
+  var colWidth = 40
+
+  // print out the total for each country, one column at a time
+  x = 200
+  for (var r=0; r<table.getRowCount(); r++){
+    y = 100
+    for (var c=1; c<table.getColumnCount(); c++){
+      var value = table.getNum(r, c)
+      text(value, x, y)
+      y += rowHeight
+    }
+    x += colWidth
+  }
+
+
+
 }
