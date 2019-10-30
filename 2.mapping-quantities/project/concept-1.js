f633b8e34dfae12119c6fe2c9cbec06cbc9fcb05
Zac Zhibang Jiang <zhibang.jiang@gmail.com>
2019-10-30 18:09:07 -0400

    Update concept-1.js

--- 2.mapping-quantities/project/concept-1.js
+++ 2.mapping-quantities/project/concept-1.js
@@ -24,5 +24,36 @@ function setup(){
   var y = 100
   var rowHeight = 60
   var colWidth = 40
-  
+
+  // draw the frequency label on the left edge of the table
+  textStyle(BOLD)
+  textAlign(RIGHT)
+  for (var c=1; c<table.getRowCount(); c++){
+    text(table.columns[c], x-colWidth, y)
+    y += rowHeight
+  }
+
+  // draw year labels in the header row
+  x = 200
+  y = 100
+  textStyle(NORMAL)
+  textAlign(BOLD)
+  for (var r=0; r<table.getColumnCount(); r++){
+    var year = table.getString(r, 0)
+    text(year, x, y-rowHeight)
+    x += colWidth
+  }
+
+  // print out the total for each year, one column at a time
+  x = 200
+  for (var r=0; r<table.getRowCount(); r++){
+    y = 100
+    for (var c=1; c<table.getRowCount(); c++){
+      var value = table.getNum(r, c)
+      text(value, x, y)
+      y += rowHeight
+    }
+    x += colWidth
+  }
+
 }
