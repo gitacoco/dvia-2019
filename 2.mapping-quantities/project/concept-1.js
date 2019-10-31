0a4ab50409037949c37a43ea318df4f15bb90e63
Zac Zhibang Jiang <zhibang.jiang@gmail.com>
2019-10-30 21:37:34 -0400

    Update

--- 2.mapping-quantities/project/concept-1.js
+++ 2.mapping-quantities/project/concept-1.js
@@ -12,8 +12,7 @@ function setup(){
   var table = totals
  
   // count the colums
-  print(table.getRowCount() + ' total rows in table')
-  print(table.getColumnCount() + ' total columns in table')
+  print(table)
 
   // set up typography
   textFont("Rokkitt")
@@ -26,11 +25,37 @@ function setup(){
   var rowHeight = 60
   var colWidth = 40
 
-  print(table.getRow('Year'))
-  print(table.getRow('Frequency'))
+  // draw year labels in the header row
+  x = 200
+  y = 100
+  textStyle(NORMAL)
+  textAlign(BOLD)
+  for (var r=0; r<table.getRowCount(); r++){
+    var year = table.getString(r, 0)
+    text(year, x, y-rowHeight)
+    x += colWidth
+  }
 
-  for (let r = 0; r < table.getRowCount(); r++)
-  	for (let c = 0; c< table.getColumnCount(); c++){print(table.getString(r, c))
-  	}
+  // draw the frequency label on the left edge of the table
+  textStyle(BOLD)
+  textAlign(RIGHT)
+  for (var c=1; c<table.getColumnCount(); c++){
+    text(table.columns[c], x-colWidth, y)
+    y += rowHeight
+  }
+
+  // print out the total for each year, one column at a time
+  x = 200
+  for (var r=1; r<table.getRowCount(); r++){  
+    y = 100
+	for (var c=0; c<table.getColumnCount(); c++){
+
+      print(table.getString (r,c) )
+      var value = table.getNum(r, c)
+      text(value, x, y)
+      y += rowHeight
+    }
+    x += colWidth
+  }
 
 }
