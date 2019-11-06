dbbb0a74d262eab1cf37d24168ecd3ae7ce7ad16
Zac Zhibang Jiang <zhibang.jiang@gmail.com>
2019-11-06 15:14:07 -0500

    update for the final pre

--- 2.mapping-quantities/project/component-carbon.js
+++ 2.mapping-quantities/project/component-carbon.js
@@ -1,7 +1,7 @@
 var carbon
 
 function preload(){
-  carbon = loadTable('data/all-frequency.csv','csv', 'header')
+  carbon = loadTable('data/carbon14-ave.csv','csv', 'header')
 }
 
 function setup(){
@@ -20,33 +20,35 @@ function setup(){
   fill(30)
   noStroke()
 
-  //var x = 200
-  //var y = 100
   var rowHeight = 60
   var colWidth = 45
 
   //draw year labels
   x = 100
-  y = 100
+  y = 900
   textStyle(NORMAL)
-  textAlign(BOLD)
+  textAlign(CENTER)
   for (var r=0; r<table.getRowCount(); r++){
     var year = table.getString(r, 0)
     text(year, x, y-rowHeight)
     x += colWidth
   }  
 
-  //draw the barchart
+  //draw the scatter plot
 
   x = 100
-  y = 120
+  y = 800
   noStroke()
   fill('black')
   textStyle(NORMAL)
   textAlign(BOLD)
   for (var r=0; r<table.getRowCount(); r++){
     var tests = table.getNum(r, 1)
-    rect(x, y, colWidth-10, tests*5)
+    stroke('purple')
+    strokeWeight(10)
+    point(x, 700-tests)
+    //line(x, 700-tests, x+45, 700-tests)
     x += colWidth
   }  
+
 }
